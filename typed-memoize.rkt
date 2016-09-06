8f12224b858725df4585b73e3aa2547d823d9e57
Sourav Datta <soura.jagat@gmail.com>
2016-09-06 15:54:06 +0530

    Update typed-memoize.rkt

--- typed-memoize.rkt
+++ typed-memoize.rkt
@@ -13,4 +13,26 @@
                     result))))
     memfn))
 
-(provide memoize)
+
+(define-syntax (memoized stx)
+  (syntax-parse stx
+    ((_ type-decl (define (fn-name:id arg:id ...+) body ...+))
+     (let ([type-decl-v (syntax->datum #'type-decl)])
+       (if (not (and (list? type-decl-v)
+                     (eqv? (car type-decl-v) ':)))
+           (error "Bad type declaration!")
+           (let* ([new-fn-name (gensym)]
+                  [old-fn-name (syntax->datum #'fn-name)]
+                  [new-type-decl-v (map (lambda (s) (if (equal? s old-fn-name) new-fn-name s)) type-decl-v)])
+             (with-syntax ([fn-temp-name (datum->syntax stx new-fn-name)]
+                           [new-type-decl (datum->syntax stx new-type-decl-v)])
+               #'(begin
+                   new-type-decl
+                   (define (fn-temp-name arg ...) body ...)
+                   type-decl
+                   (define fn-name (memoize fn-temp-name))))))))
+    ((_ (define (fn-name:id arg:id ...+) body ...+))
+     #'(define fn-name (memoize (lambda (arg ...) body ...))))))
+
+(provide memoize memoized)
+
