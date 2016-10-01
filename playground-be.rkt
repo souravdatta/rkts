7705e4b8b53ca2e7d651050fe36dbe65ca55f3e4
souravdatta <soura.jagat@gmail.com>
2016-10-01 15:58:25 +0530

    More functions

--- playground-be.rkt
+++ playground-be.rkt
@@ -1,5 +1,10 @@
 #lang typed/racket
 
+(require/typed racket/tcp
+               [opaque TCP-Listener tcp-listener?]
+               [tcp-listen (-> Number TCP-Listener)]
+               [tcp-accept (-> TCP-Listener (Values Number Number))])
+               
 
 (: base-ns Namespace)
 (define base-ns (make-base-namespace))
@@ -32,3 +37,30 @@
 (: line-eval-list (-> Line-info-list Line-info-list))
 (define (line-eval-list infos)
   (map (lambda ([lin : Line-info]) (line-eval lin base-ns)) infos))
+
+
+(: read-line-info-list (-> Any Line-info-list))
+(define (read-line-info-list line)
+  (if (list? line)
+      (map (lambda (e)
+             (if (and
+                  (list? e)
+                  (= (length e) 3))
+                 (let ([n1 (first e)]
+                       [n2 (second e)]
+                       [n3 (third e)])
+                   (if (and (number? n1)
+                            (number? n2))
+                       (line-info n1 n2 n3)
+                       (error "not a valid line-info list - (number number any)")))
+                 (error "not a valid line-info list - (number number any)")))
+                 
+           line)
+      (error "should be a list of line-infos")))
+
+(: server (-> Number Void))
+(define (server port)
+  (let ([listener (tcp-listen port)])
+    (define-values (inp outp) (tcp-accept listener))
+    (displayln inp)
+    (displayln outp)))
