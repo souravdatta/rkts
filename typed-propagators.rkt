7069e1d0a17d313436537f9a75dfa74b1503cf64
Sourav Datta <soura.jagat@gmail.com>
2016-11-07 21:48:34 +0530

    Update typed-propagators.rkt

--- typed-propagators.rkt
+++ typed-propagators.rkt
@@ -4,6 +4,12 @@
 (define-type Watcher (-> Void))
 (define-type (Cell a) (MPairof a (Listof Watcher)))
 
+(: true-value (-> Symbol))
+(define (true-value) 'true)
+
+(: false-value (-> Symbol))
+(define (false-value) 'false)
+
 (: fire-watchers (All (a) (-> (Cell a) Void)))
 (define (fire-watchers c)
   (for ([w (mcdr c)])
@@ -27,10 +33,10 @@
 (define (add-watcher! c w) (set-mcdr! c (cons w (mcdr c))))
 
 (: make-connector (All (r b ...)
-                (-> (-> b ... b r)
-                    (-> (Cell r)
-                        (Cell b) ... b
-                        Void))))
+                       (-> (-> b ... b r)
+                           (-> (Cell r)
+                               (Cell b) ... b
+                               Void))))
 (define (make-connector fn)
   (: connect (-> (Cell r) (Cell b) ... b Void))
   (define (connect output . more-inputs)
@@ -43,12 +49,43 @@
     (void))
   connect)
 
-(provide make-cell
+(: propagator (All (b ...) (-> Watcher
+                               (Cell b) ... b
+                               Void)))
+(define (propagator w . cells)
+  (: add-watcher-for (All (x) (-> (Cell x) Void)))
+  (define (add-watcher-for c)
+    (add-watcher! c w)
+    (fire-watchers c)
+    (void))
+  (map add-watcher-for cells)
+  (void))
+
+(: conditional (All (a) (-> (Cell Symbol)
+                            (Cell a)
+                            (Cell a)
+                            (Cell a)
+                            Void)))
+(define (conditional pred-cell out-cell t-cell f-cell)
+  (propagator (lambda ()
+                (let ((v (get-value pred-cell)))
+                  (cond
+                    ((equal? v (true-value)) (set-value! out-cell (get-value t-cell)))
+                    ((equal? v (false-value)) (set-value! out-cell (get-value f-cell)))
+                    (else (set-value! out-cell (get-value out-cell))))))
+              pred-cell t-cell f-cell))             
+
+
+(provide true-value
+         false-value
+         make-cell
          get-value
          set-value!
          get-watchers
          add-watcher!
-         make-connector)
+         make-connector
+         propagator
+         conditional)
 
 #|
 (define c1 (make-cell 10))
@@ -57,4 +94,11 @@
 
 (define plusc (make-connector (lambda ([x : Integer] [y : Integer]) (+ x y 10))))
 (plusc c3 c1 c2)
+
+(define b1 (make-cell (false-value)))
+(define b2 (make-cell 1))
+(define b3 (make-cell 11))
+(define b4 (make-cell 0))
+(conditional b1 b4 b2 b3)
 |#
+
