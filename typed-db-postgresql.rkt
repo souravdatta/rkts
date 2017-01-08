5ab30f6993e06d4a05b26a8503d3c6e5a4286aab
Sourav Datta <soura.jagat@gmail.com>
2017-01-08 21:54:31 +0530

    Create typed-db-postgresql.rkt
    A typed version of postgredql-connect for typed/racket, till it becomes available officially.

---
+++ typed-db-postgresql.rkt
@@ -0,0 +1,23 @@
+#lang typed/racket
+
+(require typed/openssl)
+
+(require typed/db)
+
+(require/typed db
+               (postgresql-connect (->* (#:user String
+                                         #:database String)
+                                        (#:allow-cleartext-password? Boolean
+                                         #:debug? Any
+                                         #:notice-handler (U 'output 'error Output-Port (-> String String Any))
+                                         #:notification-handler (U 'output 'error Output-Port (-> String Any) (-> String String Any))
+                                         #:server String
+                                         #:port Integer
+                                         #:socket (U String 'guess False)
+                                         #:password (U String False)
+                                         #:ssl (U 'yes 'optional 'no)
+                                         #:ssl-context SSL-Client-Context)
+                                        Connection)))
+
+
+(provide postgresql-connect)
