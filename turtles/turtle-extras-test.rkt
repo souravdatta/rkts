81e487ee4dca4b766521b65d127b2e42004a5e31
Sourav Datta <soura.jagat@gmail.com>
2017-03-17 00:19:40 +0530

    Turtles

---
+++ turtles/turtle-extras-test.rkt
@@ -0,0 +1,11 @@
+#lang typed/racket
+
+(require "turtle-extras.rkt")
+
+(turtles-on)
+
+(fd 100)
+(right 90)
+(arc-left 180 50)
+
+
---
+++ turtles/turtle-extras.rkt
@@ -0,0 +1,41 @@
+#lang typed/racket
+
+(require/typed graphics/turtles
+               [draw (-> Real Void)]
+               [turn (-> Real Void)]
+               [turtles (-> Boolean Void)])
+
+(: arc-left (-> Real Real Void))
+(define (arc-left angle radius)
+  (for ([x (in-range angle)])
+    (draw (/ (* 2 3.14 radius) 360))
+    (turn 1)))
+
+(: arc-right (-> Real Real Void))
+(define (arc-right angle radius)
+  (for ([x (in-range angle)])
+    (draw (/ (* 2 3.14 radius) 360))
+    (turn (- 1))))
+
+(: fd (-> Real Void))
+(define (fd x)
+  (draw x))
+
+(: bk (-> Real Void))
+(define (bk x)
+  (draw (- x)))
+
+(: right (-> Real Void))
+(define (right x)
+  (turn (- x)))
+
+(: left (-> Real Void))
+(define (left x)
+  (turn x))
+
+(: turtles-on (-> Void))
+(define (turtles-on)
+  (turtles #t)
+  (left 90))
+
+(provide (all-defined-out))
